7c79414e0ab263309c00d378be18516dbd28fc09
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 00:48:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,21 @@
+
 def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-
-  
 def batch_badge_creator(attendees)
- while attendees.size > 0
- attendees.push do | name |
-  "Hello, my name is #{name}."
-  end
+   attendees.map do | name |
+    "Hello, my name is #{name}."
+end
 end
 
+def assign_rooms(attendees)
+  attendees.map do | name |
+    "Hello, #{name}! You'll be assigned to room #{attendees.index(name)+1}!"
+
+end
+end
+
+def printer(attendees)
+   puts batch_badge_creator(attendees)
+end
